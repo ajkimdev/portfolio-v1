08eca022e86d2ddcc847a4aab781e0f02352838e
ajkimdev <ajkim.dev@gmail.com>
2020-05-18 11:36:35 -0700

    Initial commit

---
+++ src/pages/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function index() {
+    return (
+        <div style = {{ color: 'teal'}}>
+            <h1>Hello, Portfolio-v1</h1>
+        </div>
+    )
+}
+
+export default index
