49768379a109fb5c178ce221be60c0c81197c54e
ajkimdev <ajkim.dev@gmail.com>
2020-05-21 23:59:07 -0700

    Update About Me page

--- src/pages/about.js
+++ src/pages/about.js
@@ -3,19 +3,21 @@ import React from "react";
 function about() {
   return (
     <div id="about" className="py-8 min-h-screen">
-      <h1>This is about</h1>
+      <h1>About Me</h1>
       <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
+      <div className="flex flex-wrap">
+        <div className="w-1/2 pr-12">
+          <p>
+            I like building awesome software. I've built websites, browser
+            plugins, corporate software and even games! If you are interested,
+            you can view some of my favorite projects in my portfolio down
+            below.
+          </p>
+        </div>
+        <div className="w-1/2">
+          <img src="https://picsum.photos/300" alt="profile" />
+        </div>
+      </div>
     </div>
   );
 }
